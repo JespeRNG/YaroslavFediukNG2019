321fd9f0fae1f374c084dce01f650ca59a1d6d2c
JespeRNG <viacheslavovich25122001@gmail.com>
2019-09-16 23:53:51 +0300

    Added the first task

---
+++ Lect_1/task111/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout << "Input please A number: ";
+    cin >> a;
+    cout << "Input please B number: ";
+    cin >> b;
+    cout << "The result is: "<<a+b;
+    //Чисто красоты ради
+    cout<<endl;
+}
