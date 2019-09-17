3293dea2e78e555a220e4b3ceb6129d1df299f75
JespeRNG <viacheslavovich25122001@gmail.com>
2019-09-18 00:11:31 +0300

    Added task125

---
+++ Lect_1/task125/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, M, K;
+
+    cout << "Belochka sobrala shishek: ";
+    cin >> N;
+
+    cout << "Belochka sobrala oreshkov iz shishek: ";
+    cin >> M;
+
+    cout << "Belochke nuzhno oreshkov dlya zhizni: ";
+    cin >> K;
+    cout <<endl;
+
+    if (N > 0 || M > N || M < K){
+        if (M <= N){
+            if (M >= K){
+                cout << "Belochka provedet otlichnuyu zimu :D" << endl;
+            }
+        }
+    }else cout << "U belochki proizoshel bug" << endl;
+
+}
