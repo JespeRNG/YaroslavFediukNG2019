e4b774a96e550677c7b2fd9b68bc204330287e37
JespeRNG <viacheslavovich25122001@gmail.com>
2019-09-17 23:59:16 +0300

    Added task124

---
+++ Lect_1/task124/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cout << "Input A number: ";
+    cin >> a;
+    cout << "Input B number: ";
+    cin >> b;
+    cout << "Input C number: ";
+    cin >> c;
+
+    if (a*b==c && a*c==b && b*c==a){
+        cout << "YES";
+    }else cout << "NO" << endl;
+}
