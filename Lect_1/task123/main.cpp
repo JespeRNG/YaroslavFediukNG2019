cf841d27a152e872d530926f723561b3d31f728a
JespeRNG <viacheslavovich25122001@gmail.com>
2019-09-19 16:59:45 +0300

    Removed 'salary' words

--- Lect_1/task123/main.cpp
+++ Lect_1/task123/main.cpp
@@ -6,20 +6,20 @@ int main()
 {
     int a, b, c, maxs;
 
-    cout << "Input please the 1-st salary: ";
+    cout << "Input please the 1-st number: ";
     cin >> a;
     maxs = a;
 
-    cout << "Input please the 2-nd salary: ";
+    cout << "Input please the 2-nd number: ";
     cin >> b;
         if (b > maxs){
             maxs = b;
         }
-    cout << "Input please the 3-rd salary: ";
+    cout << "Input please the 3-rd number: ";
     cin >> c;
         if (c > maxs){
             maxs = c;
         }
     cout << endl;
-    cout << "The biggest salary is " << maxs << endl;
+    cout << "The biggest number is " << maxs << endl;
 }
