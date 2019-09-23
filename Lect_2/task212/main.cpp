76868aeafdb0852331f9a24b05bc4e7c387f7534
JespeRNG <viacheslavovich25122001@gmail.com>
2019-09-23 23:50:59 +0300

    Added the second task212

---
+++ Lect_2/task212/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Input please an amount of numbers: ";
+    cin >> n;
+
+    int i=0;
+
+    while ( i < n+1){
+        cout << i;
+        i++;
+    }
+}
