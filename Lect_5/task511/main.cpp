d0ac50fe0d1e77ae1dd4e2e52372c1805e26bc4d
JespeRNG <viacheslavovich25122001@gmail.com>
2019-10-22 14:40:38 +0300

    Updated task 5.1.1. Removed pseudo-dynamic array and made it in another way

--- Lect_5/task511/main.cpp
+++ Lect_5/task511/main.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-void multiply(int digit, int arr[], int length){
+void multiply(int digit, int arr[]){
 
     int i = 0;
 
     cout << endl;
     cout << "Now, your array looks like that: " << endl;
 
-    while (i < length){
+    while (i < 6){
         arr[i] = arr[i]*digit;
         cout << i << " element is: " << arr[i] << "," << endl;
         i++;
@@ -19,17 +19,14 @@ void multiply(int digit, int arr[], int length){
 
 int main()
 {
-    int l=0;
     int i =0;
     int multi = 0;
+    int d[6];
 
-    cout << "Enter please a length of array: ";
-    cin >> l;
-    int d[l];
-    cout << "\nEnter please digits in this array." << endl;
+    cout << "Enter please digits in this array." << endl;
 
-    while (i < l){
-        cout << "Digit of " << i << " element: ";
+    while (i < 6){
+        cout << "Digit of " << i+1 << " element: ";
         cin >> d[i];
         i++;
     }
@@ -40,5 +37,5 @@ int main()
     cin >> multi;
     cout << endl;
 
-    multiply(multi, d, l);
+    multiply(multi, d);
 }
