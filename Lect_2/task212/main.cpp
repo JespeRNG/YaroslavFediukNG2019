3357d284ddf2d7b0407ca969f4581eaa61df9f0d
JespeRNG <viacheslavovich25122001@gmail.com>
2019-10-23 16:04:17 +0300

    Update task 2.1.2 and did it without 'if()'

--- Lect_2/task212/main.cpp
+++ Lect_2/task212/main.cpp
@@ -5,19 +5,16 @@ using namespace std;
 int main()
 {
     int n;
+    do{
+        cout << "Input please an amount of numbers: ";
+        cin >> n;
+    }while (n < 1);
 
-    cout << "Input please an amount of numbers: ";
-    cin >> n;
-
-    int i=0;
-
-    while ( i < n+1){
-        cout << i;
-
-        if (i == n){
-            cout << ".";
-        }else cout << ", ";
-
-        i++;
+    for (int i=0; i < n; i++){
+        cout << i << ", ";
     }
+
+    cout << n << ".";
+
+    cout << endl;
 }
