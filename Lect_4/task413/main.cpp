2138c7a1f23b3f0ccb765815b5cdbf5a04f57497
JespeRNG <viacheslavovich25122001@gmail.com>
2019-10-24 18:05:27 +0300

    Updated task 4.1.3

--- Lect_4/task413/main.cpp
+++ Lect_4/task413/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    char sentence[100];
+    /**char sentence[100];
     int SENTENCE[100];
     int i = 0, cnt = 0;
 
@@ -33,6 +33,30 @@ int main()
     while (i < cnt){
         cout << char(SENTENCE[i]);
         i++;
+    }
+
+    cout << endl;**/
+
+    char sentence[100];
+    int i = 0, n = 0;
+    char alpha[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M',
+    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+    cout << "Enter your sentence please: ";
+    cin.getline(sentence, 100);
+
+    while (sentence[i] != NULL){
+        if (sentence[i] == ' ' || sentence[i] == ',' || sentence[i] == '.' || sentence[i] >= 62 && sentence[i] <= 90){
+        }else sentence[i] = int(sentence[i])-32;
+
+        i++;
+    }
+
+    cout << endl;
+
+    while (n < i){
+        cout << sentence[n];
+        n++;
     }
 
     cout << endl;
