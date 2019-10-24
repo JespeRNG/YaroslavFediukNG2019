8885273da07957dab9c4ba1086ca7ea3a7fb3f28
JespeRNG <viacheslavovich25122001@gmail.com>
2019-10-24 18:06:57 +0300

    Removed commentaries in task 4.1.3

--- Lect_4/task413/main.cpp
+++ Lect_4/task413/main.cpp
@@ -4,39 +4,6 @@ using namespace std;
 
 int main()
 {
-    /**char sentence[100];
-    int SENTENCE[100];
-    int i = 0, cnt = 0;
-
-    cout << "Input please a sentence: ";
-    cin.getline(sentence, 100);
-
-    while (sentence[i] != NULL){
-
-        if (sentence[i] != ' ' || sentence[i] != NULL){
-            SENTENCE[i] = int(sentence[i])-32;
-
-        }
-
-        if (sentence[i] == ' '){
-            SENTENCE[i] = int(sentence[i]);
-        }
-
-        i++;
-        cnt++;
-    }
-
-    i = 0;
-
-    cout << endl;
-
-    while (i < cnt){
-        cout << char(SENTENCE[i]);
-        i++;
-    }
-
-    cout << endl;**/
-
     char sentence[100];
     int i = 0, n = 0;
     char alpha[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M',
