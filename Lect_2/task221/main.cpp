99db5dbd85e589f72c6132492ac1bd03c83ef8a8
JespeRNG <viacheslavovich25122001@gmail.com>
2019-10-28 13:26:39 +0200

    Removed (else)

--- Lect_2/task221/main.cpp
+++ Lect_2/task221/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int schools[10], your_s;
     int i = 0;
-    int yes_no = 0;
+    bool yes_no = 0;
 
     while (i < 10){
         cout << "Enter please number of the " << i+1 << " school: ";
@@ -20,13 +20,9 @@ int main()
     cin >> your_s;
 
     for (int n=0; n < i; n++){
-        if (schools[n] != your_s){
-            continue;
-        }else{
-            cout << "\nYeah, I remember this school :)\n";
-            yes_no = 1;
-        }
+        if(schools[n] == your_s) yes_no = 1;
     }
 
-    if (yes_no != 1) cout << "\nUnfortunately, I don't remember this school :(\n";
+    if (yes_no == 0) cout << yes_no << "\nUnfortunately, I don't remember this school :(\n";
+    if (yes_no == 1) cout << "\nYeah, I remember this school :)\n";
 }
