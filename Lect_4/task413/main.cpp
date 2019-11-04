1bbfdb690ec57237aa7f9216f49b673cba0a1dec
JespeRNG <viacheslavovich25122001@gmail.com>
2019-11-04 12:49:00 +0200

    Fixed task 4.1.3 and optimized it

--- Lect_4/task413/main.cpp
+++ Lect_4/task413/main.cpp
@@ -5,25 +5,15 @@ using namespace std;
 int main()
 {
     char sentence[100];
-    int i = 0, n = 0;
-    char alpha[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M',
-    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    int i = 0;
 
-    cout << "Enter your sentence please: ";
+    cout << "Enter please your sentence: ";
     cin.getline(sentence, 100);
 
     while (sentence[i] != NULL){
-        if (sentence[i] == ' ' || sentence[i] == ',' || sentence[i] == '.' || sentence[i] >= 62 && sentence[i] <= 90){
-        }else sentence[i] = int(sentence[i])-32;
-
+        if (sentence[i] >= 97 && sentence[i] <= 122) { cout << char(sentence[i]-32);
+        }else cout << sentence[i];
         i++;
-    }
-
-    cout << endl;
-
-    while (n < i){
-        cout << sentence[n];
-        n++;
     }
 
     cout << endl;
